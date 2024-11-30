a2fcd5d1d02c876350579f920eb3eb86797a0c6f
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-12-01 00:24:27 +0300

    model layer changed to 5 with less density

--- backend/app/models/simple_nn.py
+++ backend/app/models/simple_nn.py
@@ -1,22 +1,19 @@
 import torch
 import torch.nn as nn
 
-
 class SimpleNN(nn.Module):
     def __init__(self):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(3, 8)
-        self.fc2 = nn.Linear(8, 8)
-        self.fc3 = nn.Linear(8, 6)
-        self.fc4 = nn.Linear(6, 6)
-        self.fc5 = nn.Linear(6, 4)
-        self.fc6 = nn.Linear(4, 2)
+        self.fc1 = nn.Linear(3, 16)
+        self.fc2 = nn.Linear(16, 12)
+        self.fc3 = nn.Linear(12, 8)
+        self.fc4 = nn.Linear(8, 4)
+        self.fc5 = nn.Linear(4, 2)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
         x = torch.relu(self.fc4(x))
-        x = torch.relu(self.fc5(x))
-        x = self.fc6(x)
+        x = self.fc5(x)
         return x
