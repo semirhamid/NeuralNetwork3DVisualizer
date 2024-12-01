95bfe236084acf3b749d9ba7201ecba21417bc1e
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-12-01 23:42:43 +0300

    added favicon

--- backend/app/models/simple_nn.py
+++ backend/app/models/simple_nn.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class SimpleNN(nn.Module):
     def __init__(self):
         super(SimpleNN, self).__init__()
+        self.fc0 = nn.Linear(3, 3)
         self.fc1 = nn.Linear(3, 16)
         self.fc2 = nn.Linear(16, 12)
         self.fc3 = nn.Linear(12, 8)
@@ -11,6 +12,7 @@ class SimpleNN(nn.Module):
         self.fc5 = nn.Linear(4, 2)
 
     def forward(self, x):
+        x = torch.relu(self.fc0(x))
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
